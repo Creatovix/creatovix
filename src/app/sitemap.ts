689cce98d0dd2d7c12f9e046752480f333caeab0
Mr-Shehroz <mr.shehroz.khan2006>
2026-05-12 23:23:09 +0500

    add open graph urls for portfolio and blog

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -38,6 +38,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     {
+      url: `${baseUrl}/services`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/blog`,
       lastModified: new Date(),
       changeFrequency: "weekly",
